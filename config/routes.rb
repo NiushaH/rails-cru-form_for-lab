c86575f56781624c5c5a34d4b703e7a83aae3e7d
Niusha H <niusha.hadziomerovic@gmail.com>
2021-12-16 14:00:22 -0700

    commit tree abba7bcd87f41f0cc1f6ac2b6015945c1fb7d225

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,37 @@
 Rails.application.routes.draw do
+  resources :songs
+  resources :genres
+  resources :artists
+  
+  # get 'songs/new'
+
+  # get 'songs/create'
+
+  # get 'songs/edit'
+
+  # get 'songs/update'
+
+  # get 'songs/show'
+
+  # get 'genres/new'
+
+  # get 'genres/create'
+
+  # get 'genres/edit'
+
+  # get 'genres/update'
+
+  # get 'genres/show'
+
+  # get 'artists/new'
+
+  # get 'artists/create'
+
+  # get 'artists/edit'
+
+  # get 'artists/update'
+
+  # get 'artists/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
